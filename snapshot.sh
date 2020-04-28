f1f39d74c169911771d8c7c3276a456b8837ce7c
Oliver Gutsche <oguatworld@gmail.com>
2020-04-28 17:57:15 -0500

    switched script to raspistill

--- snapshot.sh
+++ snapshot.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-ffmpeg -y -loglevel fatal -i rtsp://<replace me username>:<replace me passwd>@localhost:8554/stream -vframes 1 -r 1 /var/www/html/snapshot.jpg
+# ffmpeg -y -loglevel fatal -i rtsp://<replace me username>:<replace me passwd>@localhost:8554/stream -vframes 1 -r 1 /var/www/html/snapshot.jpg
+# Downstairs and Garage: raspistill -h 270 -w 480 -q 50 -rot 90 -o /var/www/html/snapshot.jpg
+# Upstairs: raspistill -h 270 -w 480 -q 50 -o /var/www/html/snapshot.jpg
